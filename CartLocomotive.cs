4b46827cb32d49a66010ec0ae22a1985db11a2e8
Dmitriy <apelsinka.channel@gmail.com>
2017-12-16 22:37:17 +0400

    Add files via upload

---
+++ CartLocomotive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class CartLocomotive : Locomotive
+    {
+        private bool pipe;
+        private bool coal;
+        private Color dopColor;
+
+        public CartLocomotive(int maxSpeed, int maxCountPassengers, double weight, Color color, bool pipe, bool coal, Color dopcolor) : base(maxSpeed, maxCountPassengers, weight, color)
+        {
+            this.pipe = pipe;
+            this.coal = coal;          
+            this.dopColor = dopColor;
+        }
+
+        protected override void drawBaseLocomotive(Graphics g)
+        {
+            if (pipe)
+            {
+                Pen pen = new Pen(Color.Black);
+                g.DrawEllipse(pen, startPosX + 80, startPosY - 6, 20, 20);
+            }
+
+            if (coal)
+            {
+                Pen pen = new Pen(Color.Black);
+                g.DrawEllipse(pen, startPosX + 80, startPosY - 6, 30, 30);
+            }
+            base.drawBaseLocomotive(g);       
+
+        }
+
+
+    }
+}
---
+++ Locomotive.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Locomotive : Vehicle
+    {
+
+        public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 100)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 50;
+                }
+            }
+        }
+
+        public override int MaxCountPassengers
+        {
+            get
+            {
+                return base.MaxCountPassengers;
+            }
+
+            protected set
+            {
+                if (value > 1 && value < 100)
+                {
+                    base.MaxCountPassengers = value;
+                }
+                else
+                {
+                    base.MaxCountPassengers = 50;
+                }
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+
+            protected set
+            {
+
+                if (value > 5000 && value < 10000)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 7500;
+                }
+            }
+        }
+
+        public Locomotive(int maxSpeed, int maxCountPassengers, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountPassengers = maxCountPassengers;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countPassengers = 0;
+            Random rand = new Random();
+            startPosX = rand.Next(10, 200);
+            startPosY = rand.Next(10, 200);
+        }
+
+
+        public override void drawLocomotive(Graphics g)
+        {
+            drawBaseLocomotive(g);
+        }
+
+        public override void moveLocomotive(Graphics g)
+        {
+            startPosX += MaxSpeed / 10;   //ПОТОМ ИСПРАВИТЬ НА ФОРМУЛУ
+        }
+
+        protected virtual void drawBaseLocomotive(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            g.DrawRectangle(pen, startPosX + 80, startPosX + 10, 10, 30);
+        }
+
+    }
+}
