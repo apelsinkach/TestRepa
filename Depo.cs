5385581b808510be404e7dec093ede9b5a29f747
Dmitriy <apelsinka.channel@gmail.com>
2017-12-16 22:40:38 +0400

    Add files via upload

---
+++ Depo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Depo
+    {
+        ClassArray<ITransport> depo;
+
+        int countPlaces = 5;
+
+        int placeSizeWidth = 300;
+
+        int placeSizeHeight = 100;
+
+        public Depo()
+        {
+            depo = new ClassArray<ITransport>(countPlaces, null);
+        }
+
+
+        public int PutLocoInDepo(ITransport locomotive)
+        {
+            return depo + locomotive;
+        }
+
+        public ITransport GetLocoInDepo(int report)
+        {
+            return depo - report;
+        }
+
+
+        public void Draw(Graphics g, int width, int height)
+        {
+            DrawMarking(g);
+            for (int i = 0; i < countPlaces; i++)
+            {
+                var locomotive = depo.getObject(i);
+                if (locomotive != null)
+                {
+                    locomotive.setPosition(5 + i / 5 * placeSizeWidth + 5, i % 5 * placeSizeHeight + 45);
+                    locomotive.drawLocomotive(g);
+                }
+
+            }
+        }
+
+        public void DrawMarking(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            g.DrawRectangle(pen, 0, 0, countPlaces / 5 * placeSizeWidth, 480);
+            for(int i = 0; i < countPlaces / 5; i++)
+            {
+                for(int j = 0; j < 6; ++j)
+                {
+                    g.DrawLine(pen,i*placeSizeWidth,j*placeSizeHeight,i*placeSizeWidth+110,j*placeSizeHeight);
+
+                }
+                g.DrawLine(pen, i * placeSizeWidth, 0, i * placeSizeWidth, 400);
+            }
+
+
+        }
+    }
+}
---
+++ ITransport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    interface ITransport
+    {
+        void moveLocomotive(Graphics g);
+
+        void drawLocomotive(Graphics g);
+
+        void setPosition(int x , int y);
+
+        void loadPassengers(int count);
+   
+
+        int getPassenger();
+    }
+}
