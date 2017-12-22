1a56fb19de9e1ce946fd590e88cefe4c068825be
Dmitriy <apelsinka.channel@gmail.com>
2017-12-22 21:01:26 +0400

    Add files via upload

---
+++ FormDepocs.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class FormDepocs : Form
+    {
+        Depo depo;
+
+        FormSelectLoco form;
+
+        public FormDepocs()
+        {
+            InitializeComponent();
+            depo = new Depo(5);
+            for (int i = 1; i < 6; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + i);
+            }
+            listBoxLevels.SelectedIndex = depo.getCurrentLevel;
+   
+            Draw();
+        }
+
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                depo.Draw(gr, pictureBoxParking.Width, pictureBoxParking.Height);
+                pictureBoxParking.Image = bmp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var locomotive = new Locomotive(100, 4, 1000, dialog.Color);
+                int place = depo.PutLocoInDepo(locomotive);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        var locomotive = new CartLocomotive(100, 4, 1000, dialog.Color, true, true, dialogDop.Color);
+                        int place = depo.PutLocoInDepo(locomotive);
+                        Draw();
+                        MessageBox.Show("Ваше место: " + place);
+                    }
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                if (listBoxLevels.SelectedIndex > -1)
+                {//Прежде чем забрать машину, надо выбрать с какого уровня будем забирать
+                    string level = listBoxLevels.Items[listBoxLevels.SelectedIndex].ToString();
+                    if (maskedTextBox1.Text != "")
+                    {
+                        var locomotive = depo.GetLocoInDepo(Convert.ToInt32(maskedTextBox1.Text));
+
+                        //Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                        //Graphics gr = Graphics.FromImage(bmp);
+                        //locomotive.setPosition(20, 50);
+                        //locomotive.drawLocomotive(gr);
+                        //pictureBoxTakeCar.Image = bmp;
+                        //Draw();
+                    }
+                    else
+                    {//иначесообщаемобэтом
+                        MessageBox.Show("Извинте, на этом месте нет машины");
+                    }
+
+                }
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            depo.LevelDown();
+            listBoxLevels.SelectedIndex = depo.getCurrentLevel;
+            Draw();
+
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            depo.LevelUp();
+            listBoxLevels.SelectedIndex = depo.getCurrentLevel;
+            Draw();
+
+        }
+
+        private void buttonSetLoco_Click(object sender, EventArgs e)
+        {
+            form = new FormSelectLoco();
+            form.AddLoco(AddLoco);
+            form.Show();
+
+            
+        }
+
+        private void AddLoco(ITransport loco)
+        {
+            if (loco != null)
+            {
+                int place = depo.PutLocoInDepo(loco);
+                if (place > -1)
+                {
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+                else
+                {
+                    MessageBox.Show("Не удалось поставить!");
+                }
+            }
+        }
+    }
+}
+
---
+++ Locomotive.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Locomotive : Vehicle
+    {
+
+        public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 150)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 50;
+                }
+            }
+        }
+
+        public override int MaxCountPassengers
+        {
+            get
+            {
+                return base.MaxCountPassengers;
+            }
+
+            protected set
+            {
+                if (value > 0 && value < 0)
+                {
+                    base.MaxCountPassengers = value;
+                }
+                else
+                {
+                    base.MaxCountPassengers = 4;
+                }
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+
+            protected set
+            {
+
+                if (value > 300 && value < 1000)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 1000;
+                }
+            }
+        }
+
+        public Locomotive(int maxSpeed, int maxCountPassengers, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountPassengers = maxCountPassengers;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countPassengers = 0;
+            Random rand = new Random();
+            startPosX = rand.Next(400, 500);
+            startPosY = rand.Next(100, 200);
+        }
+
+
+        public override void drawLocomotive(Graphics g)
+        {
+            drawBaseLocomotive(g);
+        }
+
+        public override void moveLocomotive(Graphics g)
+        {
+            startPosX -= (MaxSpeed *50/(float)Weight)/(countPassengers==0?1:countPassengers);
+            drawLocomotive(g);
+        }
+
+        protected virtual void drawBaseLocomotive(Graphics g)
+        {
+            Pen penB = new Pen(Color.Black);
+            Pen penR = new Pen(Color.Red);
+            Brush brushR = new SolidBrush(Color.Red);
+            Brush brushB = new SolidBrush(Color.Black);
+            Brush brushRB = new SolidBrush(Color.DeepPink);
+
+            g.DrawEllipse(penR, startPosX+10, startPosY + 30, 20, 20);
+            g.DrawEllipse(penR, startPosX + 25, startPosY+ 30, 20, 20);
+            g.DrawEllipse(penR, startPosX + 60, startPosY + 30, 20, 20);
+            g.DrawEllipse(penR, startPosX + 75, startPosY + 30, 20, 20);
+
+           
+
+            g.FillEllipse(brushRB, startPosX + 10, startPosY + 30, 20, 20);
+            g.FillEllipse(brushRB, startPosX + 25, startPosY + 30, 20, 20);
+            g.FillEllipse(brushRB, startPosX + 60, startPosY + 30, 20, 20);
+            g.FillEllipse(brushRB, startPosX + 75, startPosY + 30, 20, 20);
+
+
+            g.DrawRectangle(penB, startPosX, startPosY, 100, 30);
+
+            Brush LokoBrush = new SolidBrush(ColorBody);
+            g.FillRectangle(LokoBrush, startPosX, startPosY, 100, 30);
+            g.DrawRectangle(penB, startPosX-5, startPosY + 20, 10, 10);
+
+            g.FillRectangle(brushR, startPosX - 5, startPosY + 20, 10, 10);
+
+            g.DrawRectangle(penB, startPosX + 100, startPosY +20, 5, 5);
+            g.DrawRectangle(penB, startPosX + 80, startPosY + 5, 15, 15);
+            g.DrawLine(penB, startPosX+15, startPosY + 38, startPosX + 40, startPosY + 40);
+            g.DrawLine(penB, startPosX + 65, startPosY + 38, startPosX + 90, startPosY + 40);
+        }
+
+    }
+}
